53e3d1673e786b6ba825f7381d5793c6f6763e1e
tinavngo <143037396+tinavngo@users.noreply.github.com>
2023-10-11 19:13:36 -0700

    Added Ninetales and Crobat

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-alert('Hello world');
-let favoriteFood = 'Sushi' ;
-document.write(favoriteFood) ;
+let pokemonList = [
+    {name: 'Ninetales',
+     height: 1.1,
+     type: 'Fire',
+     weight: 19.9
+    },
+    
+    {name: 'Crobat',
+     height: 1.8,
+     type: ['Poison', 'Flying'],
+     weight: 75
+    }
+
+]
