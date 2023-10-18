cf4960deca253ee17aaaa0a68a84a0debbb87157
tinavngo <143037396+tinavngo@users.noreply.github.com>
2023-10-17 20:33:59 -0700

    Rearrangements
    Reorganized objects to single line of code, changed and added different Pokémon

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,11 @@
 let pokemonList = [
-    {name: 'Ninetales',
-     height: 1.1,
-     type: 'Fire',
-     weight: 19.9
-    },
+    {name: 'Ninetales', height: 1.1, type: ['Fire'], weight: 19.9},
 
-    {name: 'Crobat',
-     height: 1.8,
-     type: ['Poison', 'Flying'],
-     weight: 75
-    },
-    {name: 'Beedrill',
-    height: 1,
-    type: ['Poison', 'Bug'],
-    weight: 29.5
-    },
-    {name: 'Dewgong',
-     height: 1.7,
-     type: ['Ice', 'Water'],
-     weight: 120
-    }
-]
+    {name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'], weight: 40.5},
+
+    {name: 'Ponyta', height: 1, type: ['Fire'], weight: 30},
+
+    {name: 'Dewgong', height: 1.7, type: ['Ice', 'Water'], weight: 120},
+
+    {name: 'Dragonair', height: 4, type: ['Dragon'], weight: 16.5}
+];
