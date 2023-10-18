59dbe4861889a97bc8bd7c743dff2d5f294a4946
tinavngo <143037396+tinavngo@users.noreply.github.com>
2023-10-17 21:40:58 -0700

    1.3 task
    for loops, using conditions do indicate Pokémon with height # > 1.

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,11 @@ let pokemonList = [
 
     {name: 'Dragonair', height: 4, type: ['Dragon'], weight: 16.5}
 ];
+
+for(let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height>1)
+    {document.write(pokemonList[i].name + " " + "(height:"+ " "+ pokemonList[i].height + " ) " + "- Wow. That\'s Big!"+ "<br>") //condition indicates pokemon with # > 1 height
+
+} else 
+{document.write(pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + " ) " + "<br>"); } //condition leaves no message when pokemon height is # < 1 height
+}
