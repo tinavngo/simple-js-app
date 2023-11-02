bf53f90a736d0697edb121892789171158629cd7
tinavngo <143037396+tinavngo@users.noreply.github.com>
2023-11-02 14:35:28 -0700

    1.5 forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 let pokemonList = [
-    {name: 'Ninetales', height: 1.1, type: ['Fire'], weight: 19.9},
+    { name: 'Ninetales', height: 1.1, type: ['Fire'], weight: 19.9 },
 
-    {name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'], weight: 40.5},
+    { name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'], weight: 40.5 },
 
-    {name: 'Ponyta', height: 1, type: ['Fire'], weight: 30},
+    { name: 'Ponyta', height: 1, type: ['Fire'], weight: 30 },
 
-    {name: 'Dewgong', height: 1.7, type: ['Ice', 'Water'], weight: 120},
+    { name: 'Dewgong', height: 1.7, type: ['Ice', 'Water'], weight: 120 },
 
-    {name: 'Dragonair', height: 4, type: ['Dragon'], weight: 16.5}
+    { name: 'Dragonair', height: 4, type: ['Dragon'], weight: 16.5 }
 ];
 
-for(let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height>1)
-    {document.write(pokemonList[i].name + " " + "(height:"+ " "+ pokemonList[i].height + " ) " + "- Wow. That\'s Big!"+ "<br>") //condition indicates pokemon with # > 1 height
-
-} else 
-{document.write(pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + " ) " + "<br>"); } //condition leaves no message when pokemon height is # < 1 height
-}
+pokemonList.forEach(function(pokemon){
+    if (pokemon.height > 1) {
+        document.write (pokemon.name + " " + "(height: "  + pokemon.height +  " ) " + "-Wow. That's big!" + "<br>");
+    }else {
+        document.write (pokemon.name + " " + "(height: " + pokemon.height + " ) " + "<br>");
+    }
+    });
