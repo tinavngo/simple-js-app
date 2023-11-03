4ae0bc20cfa69f643a936ea459cd96b722488a2c
tinavngo <143037396+tinavngo@users.noreply.github.com>
2023-11-03 03:05:56 -0700

    1.5 IIFE
    Accessing to pokemonRepository before pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 let pokemonRepository = (function(){
 let pokemonList = [
     { name: 'Ninetales', height: 1.1, type: ['Fire'], weight: 19.9 },
-
     { name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'], weight: 40.5 },
-
     { name: 'Ponyta', height: 1, type: ['Fire'], weight: 30 },
-
     { name: 'Dewgong', height: 1.7, type: ['Ice', 'Water'], weight: 120 },
-
     { name: 'Dragonair', height: 4, type: ['Dragon'], weight: 16.5 }
 ];
+
 return {
     getAll: function() {
         return pokemonList;
     },
-    add: function(pokemon) {
-        pokemonList.push(pokemon);
+    add: function(item) {
+        pokemonList.push(item);
     }
 }
 })();
 
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
     if (pokemon.height > 1) {
         document.write (pokemon.name + " " + "(height: "  + pokemon.height +  " ) " + "-Wow. That's big!" + "<br>");
     }else {
