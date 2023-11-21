a924d54aef40241da46b747dabacd4493a76423f
tinavngo <143037396+tinavngo@users.noreply.github.com>
2023-11-21 10:25:11 -0800

    updated modal container and css file

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ let pokemonRepository = (function () {
   let repository = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  let modalContainer = document.querySelector('.modal-container');
+  let modalContainer = document.querySelector('#modal-container');
 
   function showModal(pokemon) {
     let modal = document.createElement('div');
